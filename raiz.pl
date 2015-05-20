fb8f7aa08ea031aefc4c20eb7765fb8349419c32
Gabriel Homsi <gabrielhomsi@gmail.com>
2015-05-20 10:15:38 -0300

    Update raiz.pl

--- raiz.pl
+++ raiz.pl
@@ -1,37 +1,3 @@
-soma(Op1, Op2, X) :-
-    X is Op1 + Op2.
-
-/*
- * Consultas: 
- * soma(2, 5, X).
- */
-
-diferenca(Op1, Op2, X) :-
-    X is Op1 - Op2.
-
-/*
- * Consultas: 
- * diferenca(10, 3, X).
- */
-
-multiplicacao(Op1, Op2, X) :-
-    X is Op1 * Op2.
-
-/*
- * Consultas: 
- * multiplicacao(9, 8, X).
- */
-
-divisao(Op1, Op2, X) :-
-    Op2 \= 0,
-    X is Op1 / Op2.
-
-/*
- * Consultas: 
- * divisao(15, 2, X).
- * divisao(123, 0, X).
- */
-
 raiz(A, B, C, R1, R2) :-
     Delta is (B * B) -4 * A * C,
     Delta >= 0,
