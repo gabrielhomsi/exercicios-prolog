a67cdb02148d41910a94e35a5a141ae67c621f65
Gabriel Homsi <gabrielhomsi@gmail.com>
2015-05-20 09:47:42 -0300

    Create recomenda_acao.pl

---
+++ recomenda_acao.pl
@@ -0,0 +1,27 @@
+entra_estrada(E) :-
+    nl,
+    write('Como estao as estradas?'),
+    nl,
+    write('(boas, esburacadas) ===>'),
+    read(E).
+
+entra_tempo(T) :-
+    nl,
+    write('Como esta o tempo?'),
+    nl,
+    write('(ensolarado, chuvoso) ===>'),
+    read(T).Q
+
+recomenda_acao(C, ESTRADAS, TEMPO) :-
+    ESTRADAS = boas, TEMPO = ensolarado -> C = 'va a praia';
+    ESTRADAS = esburacadas, TEMPO = ensolarado -> C = 'va ao parque';
+    ESTRADAS = boas, TEMPO = chuvoso -> C = 'va ao restaurante';
+    ESTRADAS = esburacadas, TEMPO = chuvoso -> C = 'fique em casa'.
+
+decisao :-
+    entra_estrada(Pe),
+    entra_tempo(Pt),
+    recomenda_acao(Z, Pe, Pt),
+    nl,
+    write('voce deve ==>'),
+    write(Z).
